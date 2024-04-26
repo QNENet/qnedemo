547f61a01c29cb55d43188e90c95ba3236da92cb
start.vaadin.com <no-reply@vaadin.com>
2024-04-26 20:50:15 +0000

    Generated project

---
+++ src/main/java/com/qnenet/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.qnenet.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/qnenet/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.qnenet.services;
+
+import org.springframework.lang.NonNullApi;
